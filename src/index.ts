260de628c102dac67bd91619ceb45d65a0fb3493
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-04 12:30:48 -0600

    Continue coding

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import * as net from "net";
 const server = net.createServer();
 server.on("connection", (socket) => {
-    console.log(socket.address());
+    let addr = socket.remoteAddress + ":" + socket.remotePort;
+    console.log(addr + " connected");
+    socket.on("data", (data) => {
+        console.log("Recieved data: " + data);
+    });
+    socket.on("close", (err) => {
+        console.log(addr + " disconnected");
+    });
+    socket.on("error", (err) => {
+
+    });
 });
 
 server.listen(7778);
